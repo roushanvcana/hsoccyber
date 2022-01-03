4be80643c252f4d2ede9f7d1db38e3cb86f7919e
anupvcana <92727970+anupvcana@users.noreply.github.com>
2022-01-03 16:31:30 +0530

    ignor file

--- admin/application/models/delete_model.php
+++
@@ -1,9 +0,0 @@
- 
-<?php
-	class delete_model extends CI_Model{   
-
-        public function delete_page($table, $id){
-            $this->db->delete($table, array('id' => $id));
-        }
-    }
-?>
--- admin/application/views/admin/template/template.php
+++
@@ -1,5 +0,0 @@
-<?php 
- $this->load->view('admin/template/header');
- $this->load->view($main_content);
- $this->load->view('admin/template/footer');
-?>
